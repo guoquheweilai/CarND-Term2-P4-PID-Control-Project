fa101dd1335f75da9f890d309c0ddba97572d6d2
ikcGitHub <ljcikc@gmail.com>
2018-06-15 11:33:42 -0400

    Complete UpdateError() and TotalError()

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,26 +12,42 @@ PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
   
-	/*****************************************************************************
+  /*****************************************************************************
   *  Initialization
   ****************************************************************************/
-  
-  // Initialize P, I, D errors
-  p_error = 0.0;
-  i_error = 0.0;
-  d_error = 0.0;
+  // TODO: Initialize errors and coefficients
+	
+  // Initialize P, I, D errors and previous error
+  p_error     = 0.0;
+  i_error     = 0.0;
+  d_error     = 0.0;
+  err_pre_cte = 0.0; // Add this to PID.h
+  err_total   = 0.0; // Add this to PID.h
 
   // Initialize P, I, D gains/coefficients
   PID::Kp = Kp;
   PID::Ki = Ki;
   PID::Kd = Kd;
-  
-  //
 }
 
 void PID::UpdateError(double cte) {
+  /*****************************************************************************
+  *  Update
+  ****************************************************************************/
+  // TODO: Update errors and coefficients
+  // Initialize P, I, D errors and previous error
+  p_error     = cte;
+  i_error    += cte;
+  d_error     = cte - err_pre_cte;
+  err_pre_cte = cte; 
 }
 
 double PID::TotalError() {
+  /*****************************************************************************
+  *  Calculate total error
+  ****************************************************************************/
+  // TODO: Calculate total error
+  err_total = Kp * p_error + Ki * i_error + Kd * d_error;
+  return err_total;
 }
 
