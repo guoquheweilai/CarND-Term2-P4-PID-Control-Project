7c14fb6249606f7dc24831cb597bf8a469b5405a
ikcGitHub <ljcikc@gmail.com>
2018-06-15 11:09:01 -0400

    Complete Init()

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,6 +11,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  
+	/*****************************************************************************
+  *  Initialization
+  ****************************************************************************/
+  
+  // Initialize P, I, D errors
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+
+  // Initialize P, I, D gains/coefficients
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
+  
+  //
 }
 
 void PID::UpdateError(double cte) {
