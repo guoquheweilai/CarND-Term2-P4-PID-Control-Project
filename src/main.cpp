1282db4ff11f377a4a49e5a44c4dc1a678297ee4
ikcGitHub <ljcikc@gmail.com>
2018-06-21 23:03:45 -0400

    Increase D gain

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ int main()
   //pid.Init(1.0, 1.0, 1.0); // Need to tune on the Simulator or using twiddle method?
   
   // PD only
-  pid.Init(0.1, 0.0, 2.0); // Need to tune on the Simulator or using twiddle method?
+  pid.Init(0.1, 0.0, 3.0); // Need to tune on the Simulator or using twiddle method?
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
