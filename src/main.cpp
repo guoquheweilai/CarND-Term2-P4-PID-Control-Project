a9764c60324005df6635966314171a68998c06eb
ikcGitHub <ljcikc@gmail.com>
2018-06-21 22:16:19 -0400

    Try PD only

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,10 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(1.0, 1.0, 1.0); // Need to tune on the Simulator or using twiddle method?
+  //pid.Init(1.0, 1.0, 1.0); // Need to tune on the Simulator or using twiddle method?
+  
+  // PD only
+  pid.Init(0.5, 0.0, 0.5); // Need to tune on the Simulator or using twiddle method?
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
